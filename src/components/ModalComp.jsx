c1d39d667549df7542464389922826ceccb85be1
gasperpb <achillespb@hotmail.com>
2022-08-10 16:56:26 -0300

    finale

--- src/components/ModalComp.jsx
+++ src/components/ModalComp.jsx
@@ -1,24 +1,53 @@
 import {
     Modal,
-    ModalHeader,
-    ModalBody,
-    ModalFooter,
     ModalOverlay,
     ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
     ModalCloseButton,
     Button,
     FormControl,
-    Formlabel,
+    FormLabel,
     Input,
     Box,
-
-} from "@chakra-ui/react"
-import { useState } from "react"
-
+} from "@chakra-ui/react";
+import { useState } from "react";
 
 const ModalComp = ({ data, setData, dataEdit, isOpen, onClose }) => {
-    const [name, setName] = useState(dataEdit.name || "")
-    const [email, setEmail] = useState(dataEdit.email || "")
+    const [name, setName] = useState(dataEdit.name || "");
+    const [email, setEmail] = useState(dataEdit.email || "");
+
+    const handleSave = () => {
+        if (!name || !email) return;
+
+        if (emailAlreadyExists()) {
+            return alert("E-mail já cadastrado!");
+        }
+
+        if (Object.keys(dataEdit).length) {
+            data[dataEdit.index] = { name, email };
+        }
+
+        const newDataArray = !Object.keys(dataEdit).length
+            ? [...(data ? data : []), { name, email }]
+            : [...(data ? data : [])];
+
+        localStorage.setItem("cad_cliente", JSON.stringify(newDataArray));
+
+        setData(newDataArray);
+
+        onClose();
+    };
+
+    const emailAlreadyExists = () => {
+        if (dataEdit.email !== email && data?.length) {
+            return data.find((item) => item.email === email);
+        }
+
+        return false;
+    };
+
     return (
         <>
             <Modal isOpen={isOpen} onClose={onClose}>
@@ -29,14 +58,36 @@ const ModalComp = ({ data, setData, dataEdit, isOpen, onClose }) => {
                     <ModalBody>
                         <FormControl display="flex" flexDir="column" gap={4}>
                             <Box>
-                                <Formlabel htmlFor="name">Nome</Formlabel>
-                                <Input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+                                <FormLabel>Nome</FormLabel>
+                                <Input
+                                    type="text"
+                                    value={name}
+                                    onChange={(e) => setName(e.target.value)}
+                                />
+                            </Box>
+                            <Box>
+                                <FormLabel>E-mail</FormLabel>
+                                <Input
+                                    type="email"
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                />
                             </Box>
                         </FormControl>
                     </ModalBody>
+
+                    <ModalFooter justifyContent="start">
+                        <Button colorScheme="green" mr={3} onClick={handleSave}>
+                            SALVAR
+                        </Button>
+                        <Button colorScheme="red" onClick={onClose}>
+                            CANCELAR
+                        </Button>
+                    </ModalFooter>
                 </ModalContent>
             </Modal>
         </>
-    )
-}
+    );
+};
+
 export default ModalComp;
